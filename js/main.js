01f797d8d63948a82c7028c4c9f5f83d3321463e
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-01-07 15:08:45 +0100

    add-script-jquery_replace-link-logo-broken

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* Hamburger menu:
+mostrare / nascondere il menu principale
+Ricordate di scrivere solo il codice JavaScript con jQuery e non modificare HTML e CSS */
+
+
+$(document).ready(function(){
+    // Il click viene fatto su i perchè altrimenti su un qualsiasi tag a si aprirebbe il menù;
+    $('.header-right a i').click(function(){
+        $('.hamburger-menu').show();
+    });
+
+    $('.hamburger-menu a i').click(function(){
+        $('.hamburger-menu').hide();
+    });
+
+});
